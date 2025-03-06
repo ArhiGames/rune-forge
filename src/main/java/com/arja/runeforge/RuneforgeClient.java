05d7dc1755a2d356804e55d7393f9f7d4bf371af
ArhiGames <arvid.hilgers@gmx.de>
2025-03-06 16:05:50 +0100

    Initial commit

---
+++ src/main/java/com/arja/runeforge/RuneforgeClient.java
@@ -0,0 +1,12 @@
+package com.arja.runeforge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RuneforgeClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
