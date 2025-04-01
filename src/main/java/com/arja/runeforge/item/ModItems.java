5c63d19cfe5d591f740793932b211212a45836b5
ArhiGames <arvid.hilgers@gmx.de>
2025-04-01 20:51:38 +0200

    Gave "kenaz" a rarity, added the item name translation for kenaz, removed unnecessary imports

--- src/main/java/com/arja/runeforge/item/ModItems.java
+++ src/main/java/com/arja/runeforge/item/ModItems.java
@@ -2,18 +2,15 @@ package com.arja.runeforge.item;
 
 import com.arja.runeforge.Runeforge;
 import com.arja.runeforge.rune.RuneItemBase;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 import java.util.function.Function;
 
@@ -22,7 +19,8 @@ public class ModItems
     public static final Item KEBAB = registerItem("kebab", Item::new, new Item.Settings()
             .food(ModFoodComponents.KEBAB_FOOD_COMPONENT, ModFoodComponents.KEBAB_CONSUMABLE_COMPONENT));
 
-    public static final Item RUNE_KENAZ = registerItem("kenaz", RuneItemBase::new, new Item.Settings());
+    public static final Item RUNE_KENAZ = registerItem("kenaz", RuneItemBase::new, new Item.Settings()
+            .rarity(Rarity.RARE));
 
     private static Item registerItem(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings)
     {
